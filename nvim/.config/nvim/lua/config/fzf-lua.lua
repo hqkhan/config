212db1888c90cff95c6585b0a0e12f47c0ff864b
Hassaan <Hassaan.Khan@ll.mit.edu>
2021-10-22 13:44:52 -0400

    Fzf-lua, treesitter updates

--- nvim/.config/nvim/lua/config/fzf-lua.lua
+++ nvim/.config/nvim/lua/config/fzf-lua.lua
@@ -1,31 +1,128 @@
 local actions = require "fzf-lua.actions"
 require'fzf-lua'.setup {
   winopts = {
-    -- split         = "new",           -- open in a split instead?
-    win_height       = 0.85,            -- window height
-    win_width        = 0.80,            -- window width
-    win_row          = 0.30,            -- window row position (0=top, 1=bottom)
-    win_col          = 0.50,            -- window col position (0=left, 1=right)
-    -- win_border    = false,           -- window border? or borderchars?
-    win_border       = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-    hl_normal        = 'Normal',        -- window normal color
-    hl_border        = 'Repeat',        -- window border color
+    -- split         = "belowright new",-- open in a split instead?
+                                        -- "belowright new"  : split below
+                                        -- "aboveleft new"   : split above
+                                        -- "belowright vnew" : split right
+                                        -- "aboveleft vnew   : split left
+    -- Only valid when using a float window
+    -- (i.e. when 'split' is not defined)
+    height           = 0.85,            -- window height
+    width            = 0.80,            -- window width
+    row              = 0.35,            -- window row position (0=top, 1=bottom)
+    col              = 0.50,            -- window col position (0=left, 1=right)
+    -- border argument passthrough to nvim_open_win(), also used
+    -- to manually draw the border characters around the preview
+    -- window, can be set to 'false' to remove all borders or to
+    -- 'none', 'single', 'double' or 'rounded' (default)
+    border           = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+    fullscreen       = false,           -- start fullscreen?
+    hl = {
+      normal         = 'Normal',        -- window normal color (fg+bg)
+      border         = 'Normal',        -- border color (try 'FloatBorder')
+      -- Only valid with the builtin previewer:
+      cursor         = 'Cursor',        -- cursor highlight (grep/LSP matches)
+      cursorline     = 'CursorLine',    -- cursor line
+      -- title       = 'Normal',        -- preview border title (file/buffer)
+      -- scrollbar_f = 'PmenuThumb',    -- scrollbar "full" section highlight
+      -- scrollbar_e = 'PmenuSbar',     -- scrollbar "empty" section highlight
+    },
+    preview = {
+      -- default     = 'bat',           -- override the default previewer?
+                                        -- default uses the 'builtin' previewer
+      border         = 'border',        -- border|noborder, applies only to
+                                        -- native fzf previewers (bat/cat/git/etc)
+      wrap           = 'nowrap',        -- wrap|nowrap
+      hidden         = 'nohidden',      -- hidden|nohidden
+      vertical       = 'down:45%',      -- up|down:size
+      horizontal     = 'right:60%',     -- right|left:size
+      layout         = 'flex',          -- horizontal|vertical|flex
+      flip_columns   = 120,             -- #cols to switch to horizontal on flex
+      -- Only valid with the builtin previewer:
+      title          = true,            -- preview border title (file/buf)?
+      scrollbar      = 'float',         -- `false` or string:'float|border'
+                                        -- float:  in-window floating border 
+                                        -- border: in-border chars (see below)
+      scrolloff      = '-2',            -- float scrollbar offset from right
+                                        -- applies only when scrollbar = 'float'
+      scrollchars    = {'█', '' },      -- scrollbar chars ({ <full>, <empty> }
+                                        -- applies only when scrollbar = 'border'
+    },
+    on_create = function()
+      -- called once upon creation of the fzf main window
+      -- can be used to add custom fzf-lua mappings, e.g:
+      --   vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", "<Down>",
+      --     { silent = true, noremap = true })
+    end,
   },
+  keymap = {
+    -- These override the default tables completely
+    -- no need to set to `false` to disable a bind
+    -- delete or modify is sufficient
+    builtin = {
+      -- neovim `:tmap` mappings for the fzf win
+      ["<F2>"]        = "toggle-fullscreen",
+      -- Only valid with the 'builtin' previewer
+      ["<F3>"]        = "toggle-preview-wrap",
+      ["<F4>"]        = "toggle-preview",
+      -- Rotate preview clockwise/counter-clockwise
+      ["<F5>"]        = "toggle-preview-ccw",
+      ["<F6>"]        = "toggle-preview-cw",
+      ["<S-down>"]    = "preview-page-down",
+      ["<S-up>"]      = "preview-page-up",
+      ["<S-left>"]    = "preview-page-reset",
+    },
+    fzf = {
+     -- fzf '--bind=' options
+      ["ctrl-z"]      = "abort",
+      ["ctrl-u"]      = "unix-line-discard",
+      ["ctrl-f"]      = "half-page-down",
+      ["ctrl-b"]      = "half-page-up",
+      ["ctrl-a"]      = "beginning-of-line",
+      ["ctrl-e"]      = "end-of-line",
+      ["alt-a"]       = "toggle-all",
+      -- Only valid with fzf previewers (bat/cat/git/etc)
+      ["f3"]          = "toggle-preview-wrap",
+      ["f4"]          = "toggle-preview",
+      ["shift-down"]  = "preview-page-down",
+      ["shift-up"]    = "preview-page-up",
+    },
+  },
+  -- use skim instead of fzf?
+  -- https://github.com/lotabout/skim
+  -- fzf_bin          = 'sk',
+  fzf_opts = {
+    -- options are sent as `<left>=<right>`
+    -- set to `false` to remove a flag
+    -- set to '' for a non-value flag
+    -- for raw args use `fzf_args` instead
+    ['--ansi']        = '',
+    ['--prompt']      = ' >',
+    ['--info']        = 'inline',
+    ['--height']      = '100%',
+    ['--layout']      = 'reverse',
+  },
+  -- fzf '--color=' options (optional)
+  --[[ fzf_colors = {
+      ["fg"] = { "fg", "CursorLine" },
+      ["bg"] = { "bg", "Normal" },
+      ["hl"] = { "fg", "Comment" },
+      ["fg+"] = { "fg", "Normal" },
+      ["bg+"] = { "bg", "CursorLine" },
+      ["hl+"] = { "fg", "Statement" },
+      ["info"] = { "fg", "PreProc" },
+      ["prompt"] = { "fg", "Conditional" },
+      ["pointer"] = { "fg", "Exception" },
+      ["marker"] = { "fg", "Keyword" },
+      ["spinner"] = { "fg", "Label" },
+      ["header"] = { "fg", "Comment" },
+      ["gutter"] = { "bg", "Normal" },
+  }, ]] 
+
   -- fzf_bin             = 'sk',        -- use skim instead of fzf?
   fzf_layout          = 'reverse',      -- fzf '--layout='
   fzf_args            = '',             -- adv: fzf extra args, empty unless adv
-  fzf_binds           = {               -- fzf '--bind=' options
-      'f2:toggle-preview',
-      'f3:toggle-preview-wrap',
-      'shift-down:preview-page-down',
-      'shift-up:preview-page-up',
-      'ctrl-d:half-page-down',
-      'ctrl-u:half-page-up',
-      'ctrl-f:page-down',
-      'ctrl-b:page-up',
-      'ctrl-a:toggle-all',
-      'ctrl-l:clear-query',
-  },
   preview_border      = 'border',       -- border|noborder
   preview_wrap        = 'nowrap',       -- wrap|nowrap
   preview_opts        = 'nohidden',     -- hidden|nohidden
@@ -64,7 +161,6 @@ require'fzf-lua'.setup {
       title           = true,         -- preview title?
       scrollbar       = true,         -- scrollbar?
       scrollchar      = '█',          -- scrollbar character
-      wrap            = false,        -- wrap lines?
       syntax          = true,         -- preview syntax highlight?
       syntax_limit_l  = 0,            -- syntax limit (lines), 0=nolimit
       syntax_limit_b  = 1024*1024,    -- syntax limit (bytes), 0=nolimit
@@ -72,14 +168,6 @@ require'fzf-lua'.setup {
       hl_cursor       = 'Cursor',     -- cursor highlight
       hl_cursorline   = 'CursorLine', -- cursor line highlight
       hl_range        = 'IncSearch',  -- ranger highlight (not yet in use)
-      keymap = {
-        toggle_full   = '<F2>',       -- toggle full screen
-        toggle_wrap   = '<F3>',       -- toggle line wrap
-        toggle_hide   = '<F4>',       -- toggle on/off (not yet in use)
-        page_up       = '<S-up>',     -- preview scroll up
-        page_down     = '<S-down>',   -- preview scroll down
-        page_reset    = '<S-left>',      -- reset scroll to orig pos
-      },
     },
   },
   -- provider setup
