d014a81b9154d7170cc16748fec29028552bf6ec
Hassaan Khan <hqkhan@amazon.com>
2022-08-08 00:05:37 +0000

    Delta theme updated

--- nvim/.config/nvim/lua/config/fzf-lua.lua
+++ nvim/.config/nvim/lua/config/fzf-lua.lua
@@ -141,14 +141,6 @@ require'fzf-lua'.setup {
     -- no need to set to `false` to disable a bind
     -- delete or modify is sufficient
     builtin = {
-      -- neovim `:tmap` mappings for the fzf win
-      ["<F2>"]        = "toggle-fullscreen",
-      -- Only valid with the 'builtin' previewer
-      ["<F3>"]        = "toggle-preview-wrap",
-      ["<F4>"]        = "toggle-preview",
-      -- Rotate preview clockwise/counter-clockwise
-      ["<F5>"]        = "toggle-preview-ccw",
-      ["<F6>"]        = "toggle-preview-cw",
       ["<C-d>"]       = "preview-page-down",
       ["<C-u>"]       = "preview-page-up",
       ["<C-r>"]       = "preview-page-reset",
@@ -156,17 +148,10 @@ require'fzf-lua'.setup {
     fzf = {
      -- fzf '--bind=' options
       ["ctrl-z"]      = "abort",
-      ["ctrl-u"]      = "unix-line-discard",
       ["ctrl-f"]      = "half-page-down",
       ["ctrl-b"]      = "half-page-up",
       ["ctrl-a"]      = "beginning-of-line",
       ["ctrl-e"]      = "end-of-line",
-      ["alt-a"]       = "toggle-all",
-      -- Only valid with fzf previewers (bat/cat/git/etc)
-      ["f3"]          = "toggle-preview-wrap",
-      ["f4"]          = "toggle-preview",
-      ["shift-down"]  = "preview-page-down",
-      ["shift-up"]    = "preview-page-up",
     },
   },
   fzf_opts = {
