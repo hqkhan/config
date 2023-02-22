65ea62613344770ce6277d7a23f2808da506b86c
Hassaan Khan <hqkhan@amazon.com>
2023-02-22 04:18:22 +0000

    removed unnecssary files tracked by yadm

--- nvim/.config/nvim/lua/plugins/devicons.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-  "nvim-tree/nvim-web-devicons",
-}
-function M.config()
-  require("nvim-web-devicons").setup({})
-end
-return M
-
--- nvim/.config/nvim/lua/plugins/rust-tools.lua
+++
@@ -1,6 +0,0 @@
-local M = {
-  "simrat39/rust-tools.nvim",
-  event = "VeryLazy",
-}
-
-return M
